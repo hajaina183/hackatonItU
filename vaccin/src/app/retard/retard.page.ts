43b5514174ff739257b7cc4695ef086171ead912
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-03-14 01:14:25 +0300

    alert

--- vaccin/src/app/retard/retard.page.ts
+++ vaccin/src/app/retard/retard.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-retard',
@@ -8,12 +9,27 @@ import { Router } from '@angular/router';
 })
 export class RetardPage implements OnInit {
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
   retour(){
     this.router.navigate(['/accueiladmin']);
+  }
+  async showAlert() {
+    await this.alertCtrl.create({
+      header: "Confirmation",
+      message: "Etes-vous sur d'envoyer 2 messages ?",
+      buttons: [
+        { text: "Oui", handler: () => {
+            console.log("yes");
+          }
+        },
+        {
+          text: "Non"
+        }
+      ]
+    }).then(res => res.present())
   }
 
 
